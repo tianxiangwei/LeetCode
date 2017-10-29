f66f96f6d6a874bdc2eb5d4ab3cf7fa340e61828
tianxiangwei <tianxiangwei@gmail.com>
2017-10-29 14:06:52 +0800

    clock in

--- Algorithm and Coding/4_MedianofSortedArray/test.cpp
+++ Algorithm and Coding/4_MedianofSortedArray/test.cpp
@@ -23,6 +23,14 @@
 #include <cmath>
 using namespace std;
 
+
+/*
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays.
+ The overall run time complexity should be O(log (m+n)).
+ 
+ *  这道题让我们求两个有序数组的中位数，而且限制了时间复杂度为O(log (m+n))，看到这个时间复杂度，自然而然的想到了应该使用二分查找法来求解。但是这道题被定义为Hard也是有其原因的，难就难在要在两个未合并的有序数组之间使用二分法，这里我们需要定义一个函数来找到第K个元素，由于两个数组长度之和的奇偶不确定，因此需要分情况来讨论，对于奇数的情况，直接找到最中间的数即可，偶数的话需要求最中间两个数的平均值。下面重点来看如何实现找到第K个元素，首先我们需要让数组1的长度小于或等于数组2的长度，那么我们只需判断如果数组1的长度大于数组2的长度的话，交换两个数组即可，然后我们要判断小的数组是否为空，为空的话，直接在另一个数组找第K个即可。还有一种情况是当K = 1时，表示我们要找第一个元素，只要比较两个数组的第一个元素，返回较小的那个即可。
+ * */
+
 double get_median1(vector<int>&A,vector<int>&B)
 {
 	multiset<int> temp;
@@ -156,6 +164,62 @@ double get_median2(vector<int>&A,vector<int>&B)
 
 }
 
+
+class Solution{
+	public:
+		double findMedianSortedrrays(vector<int> &nums1,vector<int>&nums2)
+		{
+			int total=nums1.size()+nums2.size();
+			if(total%2==1)
+				return findKth(nums1,0,nums2,0,total/2+1);
+			else
+				return((findKth(nums1,0,nums2,0,total/2)+findKth(nums1,0,nums2,0,total/2+1))/2.0);
+		}
+
+		int findKth(vector<int>nums1,int i,vector<int>nums2,int j,int k)
+		{
+			if(nums1.size()-i>nums2.size()-j) return findKth(nums2,j,nums1,i,k);
+			if(nums1.size()==i) return nums2[j+k-1];
+			if(k==1) return min(nums1[i],nums2[j]);
+			int pa=min(i+k/2,int(nums1.size())),pb=j+k-pa+i;
+			cout<<"i:"<<i<<" j:"<<j<<" k"<<k<<" pa:"<<pa<<" pb:"<<pb<<endl;
+			cout<<"nums1[pa-1]:"<<nums1[pa-1]<<" nums2[pb-1]:"<<nums2[pb-1]<<endl;
+			if(nums1[pa-1]<nums2[pb-1])
+				return findKth(nums1,pa,nums2,j,k-pa+i);
+			else if(nums1[pa-1]>nums2[pb-1])
+				return findKth(nums1,i,nums2,pb,k-pb+j);
+			else
+				return nums1[pa-1];
+		}
+};
+
+class Solution1{
+	public:
+		double findMedianSortedrrays(vector<int> &nums1,vector<int>&nums2)
+		{
+			int m=nums1.size();
+			int n=nums2.size();
+			return (findKth(nums1,nums2,(m+n+1)/2)+findKth(nums1,nums2,(m+n+2)/2))/2.0;
+		}
+
+		int findKth(vector<int>nums1,vector<int>nums2,int k)
+		{
+			int m=nums1.size(),n=nums2.size();
+			if(m>n)  return findKth(nums2,nums1,k);
+			if(m==0) return nums2[k-1];
+			if(k==1) return min(nums1[0],nums2[0]);
+			int i=min(m,k/2),j=min(n,k/2);
+			if(nums1[i]<nums2[j])
+				return findKth(nums1,vector<int>(nums2.begin()+j,nums2.end()),k-j);
+			else if(nums1[i]>nums2[j])
+				return findKth(vector<int>(nums1.begin()+i,nums1.end()),nums2,k-i);
+			else
+				return 0;
+		}
+};
+
+
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
@@ -192,9 +256,11 @@ main ( int argc, char *argv[] )
 		cout<<two_array[ix]<<" ";
 	cout<<endl;
 	
+	Solution test;
 	//value=get_median(one_array,two_array);
-	value=get_median1(one_array,two_array);
+	//value=get_median1(one_array,two_array);
 	//value=get_median2(one_array,two_array);
+	value=test.findMedianSortedrrays(one_array,two_array);
 	cout<<"the median value:"<<value<<endl;
 
 	return 0;
--- Algorithm and Coding/5_LongestPalindromicSubString/test.cpp
+++ Algorithm and Coding/5_LongestPalindromicSubString/test.cpp
@@ -15,11 +15,20 @@
  *
  * =====================================================================================
  */
+
+/*
+ * Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ *
+ *  
+ *
+ *  这道题让我们求最长回文子串，首先说下什么是回文串，就是正读反读都一样的字符串，比如 "bob", "level", "noon" 等等。那么最长回文子串就是在一个字符串中的那个最长的回文子串。LeetCode中关于回文串的题共有五道，除了这道，其他的四道为 Palindrome Number 验证回文数字， Validate Palindrome 验证回文字符串， Palindrome Partitioning 拆分回文串，Palindrome Partitioning II 拆分回文串之二，我们知道传统的验证回文串的方法就是两个两个的对称验证是否相等，那么对于找回文字串的问题，就要以每一个字符为中心，像两边扩散来寻找回文串，这个算法的时间复杂度是O(n*n)，可以通过OJ，就是要注意奇偶情况，由于回文串的长度可奇可偶，比如"bob"是奇数形式的回文，"noon"就是偶数形式的回文，两种形式的回文都要搜索
+ */
+
 #include <memory.h>
 #include <iostream>
 #include <string>
-using namespace std;
 
+using namespace std;
 
 //动态规划算法 时间复杂度O(n^2) 空间复杂度O(n^2)
 //dp[i][j]表示字符串从i到j是否是回文
@@ -103,13 +112,140 @@ string longest_palindrome1(string s)
 
 //Manachar算法 时间复杂度O(n) 空间复杂度O(n)
 
+
+//Time complexity O(n*n)
+class Solution{
+	public:
+		string LongestPalindrome(string s)
+		{
+			int startIdx=0,left=0,right=0,len=0;
+			for(int i=0;i>s.size()-1;i++)
+			{
+				if(s[i]==s[i+1])
+				{
+					left=i;
+					right=i+1;
+					searchPalindrome(s,left,right,startIdx,len);
+				}
+				left=right=i;
+				searchPalindrome(s,left,right,startIdx,len);
+			}
+			if(len==0) len=s.size();
+			return s.substr(startIdx,len);
+		}
+
+		void searchPalindrome(string s,int left,int right,int &startIdx,int &len)
+		{
+			int step=1;
+			while((left-step)>=0&&(right+step)<s.size())
+			{
+				if(s[left-step]!=s[right+step])
+					break;
+				++step;
+			}
+			int wide=right-left+2*step-1; //回文串长度
+			if(len<wide)
+			{
+				//发现新的回文串
+				len=wide;
+				startIdx=left-step+1;
+			}
+		}
+
+};
+
+
+/*
+ * 此题还可以用动态规划Dynamic Programming来解，根Palindrome Partitioning II 拆分回文串之二的解法很类似，我们维护一个二维数组dp，其中dp[i][j]表示字符串区间[i, j]是否为回文串，当i = j时，只有一个字符，肯定是回文串，如果i = j + 1，说明是相邻字符，此时需要判断s[i]是否等于s[j]，如果i和j不相邻，即i - j >= 2时，除了判断s[i]和s[j]相等之外，dp[j + 1][i - 1]若为真，就是回文串，通过以上分析，可以写出递推式如下：
+ *
+ * dp[i, j] = 1                                               if i == j
+ *
+ *            = s[i] == s[j]                                if j = i + 1
+ *
+ *                       = s[i] == s[j] && dp[i + 1][j - 1]    if j > i + 1      
+ *
+ *                       这里有个有趣的现象就是如果我把下面的代码中的二维数组由int改为vector<vector<int> >后，就会超时，这说明int型的二维数组访问执行速度完爆std的vector啊，所以以后尽可能的还是用最原始的数据类型吧。
+ */
+//DP(动态规划)算法
+class Solution1{
+	public:
+		string LongestPalindrome(string s)
+		{
+			int size=s.size();
+			int dp[size][size];
+			memset(dp,0,sizeof(dp));
+			int left=0,right=0,len=0;
+			for(int i=0;i<s.size();i++)
+			{
+				for(int j=0;j<i;j++)
+				{
+					dp[j][i]=(s[i]==s[j]&&(i-j<2||dp[j+1][i-1]));
+					if(dp[j][i]&&len<i-j+i)
+					{
+						len=i-j+1;
+						left=j;
+						right=i;
+					}
+				}
+				dp[i][i]=1;
+			}
+			return s.substr(left,right-left+1);
+		}
+};
+
+
+/*
+ * 最后要来的就是大名鼎鼎的马拉车算法Manacher's Algorithm，这个算法的神奇之处在于将时间复杂度提升到了O(n)这种逆天的地步，而算法本身也设计的很巧妙，很值得我们掌握，参见我另一篇专门介绍马拉车算法的博客Manacher's Algorithm 马拉车算法
+ */
+class Solution2{
+	public:
+		string LongestPalindrome(string s)
+		{
+			string t="$#";
+			for(int i=0;i<s.size();i++)
+			{
+				t+=s[i];
+				t+='#';
+			}
+			int p[(int)t.size()];
+			memset(p,0,sizeof(int)*t.size());
+			int id=0,mx=0,resId=0,resMx=0;
+			for(int i=0;i<t.size();i++)
+			{
+				p[i]=mx>i?min(p[2*id-i],mx-i):1;
+				while(t[i+p[i]]==t[i-p[i]]) 
+					++p[i];
+				if(mx<i+p[i])
+				{
+					mx=i+p[i];
+					id=i;
+				}
+				if(resMx<p[i])
+				{
+					resMx=p[i];
+					resId=i;
+				}
+			}
+			return s.substr((resId-resMx)/2,resMx-1);
+		}
+};
+
+
+
 int main()
 {
+	int size=10;
+	int dp[size][size];
+	cout<<sizeof(dp)<<endl;
+
 	string s="characher";
 	cout<<"the origin string:"<<s<<endl;
 	string substr;
 	//substr=longest_palindrome(s);
-	substr=longest_palindrome1(s);
+	//substr=longest_palindrome1(s);
+	Solution2 test;
+	substr=test.LongestPalindrome(s);
 	cout<<"the longest substring:"<<endl;
 	cout<<substr<<endl;
 }
+
